2176548ac709c259d3a5a70358630d7992b3fd06
SaffuanSaffery <saffuansaffery@gmail.com>
2023-11-18 17:39:10 +0800

    pb.authStore

--- src/app/login.js
+++ src/app/login.js
@@ -40,39 +40,42 @@ export default function Register() {
     };
 
     return (
-        <div className="w-screen flex flex-column justify-center border-b border-gray-900/10 pb-12 pt-5">
-            <form className="w-1/3">
-                <h2 className="text-base font-semibold leading-7 text-gray-900">Login</h2>
+        <div className='w-screen flex flex-column justify-center border-b border-gray-900/10 pb-12 pt-5'>
+            <form className='w-1/3'>
+                <h2 className='text-base font-semibold leading-7 text-gray-900'>Login</h2>
                 <h3>Login status: {authStatus}</h3>
-                <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-3">
-                        <label for="email-login" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-                        <div class="mt-2">
-                            <input id="email-login" name="email-login" type="email" autoComplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                {pb.authStore.model && 
+                    <h3>User id: {pb.authStore.model.id}</h3>
+                }
+                <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
+                    <div className='sm:col-span-3'>
+                        <label htmlFor="email-login" className='block text-sm font-medium leading-6 text-gray-900'>Email address</label>
+                        <div className='mt-2'>
+                            <input id="email-login" name="email-login" type="email" autoComplete="email" className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                             value={email}
                             onChange={ (e) => setEmail(e.target.value)}/>
                         </div>
                     </div>
-                    <div class="sm:col-span-3">
-                        <label for="password-login" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                        <div class="mt-2">
-                            <input id="password-login" name="password-login" type="password" autoComplete="password" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                    <div className='sm:col-span-3'>
+                        <label htmlFor="password-login" className='block text-sm font-medium leading-6 text-gray-900'>Password</label>
+                        <div className='mt-2'>
+                            <input id="password-login" name="password-login" type="password" autoComplete="password" className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' 
                             value={password}
                             onChange={ (e) => setPassword(e.target.value)}/>
                         </div>
                     </div>
                 </div>
-                <div className="mt-6 flex items-center justify-end gap-x-6">
+                <div className='mt-6 flex items-center justify-end gap-x-6'>
                     <button
                     type="button"
-                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
                     onClick={logout}
                     >
                     Logout
                     </button>
                     <button
                     type="button"
-                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
                     onClick={login}
                     >
                     Login
--- src/app/register.js
+++ src/app/register.js
@@ -34,40 +34,40 @@ export default function Register() {
 
     return (
 
-        <div class="w-screen flex flex-column justify-center border-b border-gray-900/10 pb-12 pt-5">
+        <div className='w-screen flex flex-column justify-center border-b border-gray-900/10 pb-12 pt-5'>
             <form className='w-1/3'>
-                <h2 class="text-base font-semibold leading-7 text-gray-900">Register Profile</h2>
+                <h2 className='text-base font-semibold leading-7 text-gray-900'>Register Profile</h2>
 
-                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-3">
-                        <label htmlFor="email-register" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-                        <div class="mt-2">
-                            <input id="email-register" name="email-register" type="email" autoComplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
+                    <div className='sm:col-span-3'>
+                        <label htmlFor="email-register" className='block text-sm font-medium leading-6 text-gray-900'>Email address</label>
+                        <div className='mt-2'>
+                            <input id="email-register" name="email-register" type="email" autoComplete="email" className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' 
                             value={email}
                             onChange={ (e) => setEmail(e.target.value)}/>
                         </div>
                     </div>
 
-                    <div class="sm:col-span-3">
-                        <label for="password-register" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                        <div class="mt-2">
-                            <input type="password" name="password-register" id="password-register" autoComplete="password-register" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                    <div className='sm:col-span-3'>
+                        <label htmlFor="password-register" className='block text-sm font-medium leading-6 text-gray-900'>Password</label>
+                        <div className='mt-2'>
+                            <input type="password" name="password-register" id="password-register" autoComplete="password-register" className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' 
                             value={password}
                             onChange={ (e) => setPassword(e.target.value)}/>
                         </div>
                     </div>
 
-                    <div class="sm:col-span-4">
-                        <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
-                        <div class="mt-2">
-                            <input type="text" name="username" id="username" autoComplete="username" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                    <div className='sm:col-span-4'>
+                        <label htmlFor="username" className='block text-sm font-medium leading-6 text-gray-900'>Username</label>
+                        <div className='mt-2'>
+                            <input type="text" name="username" id="username" autoComplete="username" className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' 
                             value={username}
                             onChange={ (e) => setUsername(e.target.value)}/>
                         </div>
                     </div>
                 </div>
-                <div class="mt-6 flex items-center justify-end gap-x-6">
-                    <button type="button" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={register}>Register</button>
+                <div className='mt-6 flex items-center justify-end gap-x-6'>
+                    <button type="button" className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' onClick={register}>Register</button>
                 </div>
             </form>
         </div>
