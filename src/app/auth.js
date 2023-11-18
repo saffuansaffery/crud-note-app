b28e864753c1ae91cf78674891ff394ad0b9dd40
SaffuanSaffery <saffuansaffery@gmail.com>
2023-11-18 11:04:45 +0800

    create new user added

--- src/app/auth.js
+++ src/app/auth.js
@@ -1,45 +1,58 @@
-// 'use client'
+'use client'
+import { useState } from 'react';
 import PocketBase from 'pocketbase';
-// import { useState } from "react";
 
+export default function Auth() {
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
 
-
-export default function auth(){
-    // const [email, setEmail] = useState("")
-    // const [password, setPassword] = useState("")
     const pb = new PocketBase('http://127.0.0.1:8090');
-    // const login = async() => {
-    //     try {
-    //         const authData = await pb.collection('users').authWithPassword(email, password);
-    //         console.log(pb.authStore.isValid);
-    //         console.log(pb.authStore.token);
-    //         console.log(pb.authStore.model.id);
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
-    return(
-        <>
-            <h1>Logged in : {pb.authStore.isValid.toString()} </h1>
+
+    const register = async () => {
+    try {
+        console.log(email);
+        console.log(password)
+        const data = {
+            "username": "test_username",
+            "email": email,
+            "emailVisibility": true,
+            "password":password,
+            "passwordConfirm": "12345678",
+            "name": "test"
+        };
+        // Create a new user record
+        const record = await pb.collection('users').create(data);
+        console.log('User record created:', record);
+
+        // Request email verification
+        await pb.collection('users').requestVerification(data.email);
+        console.log('Verification request sent.');
+        } catch (error) {
+        console.error('Error during registration:', error);
+      // Handle error appropriately (e.g., display an error message to the user)
+        }
+    };
+
+    return (
+        <div>
             <form>
-                <label>Email: </label>
-                <input 
-                type='text'
-                placeholder='Enter your email'
-                // value={email}
-                // onChange={ (e) => setEmail(e.target.value) }
-                >
-                </input>
-                <label>Password: </label>
-                <input
-                type='text'
-                placeholder='Enter your password'
-                // value={password}
-                // onChange={ (e) => setPassword(e.target.value) }
-                >
-                </input>
-                <button type='button'>Log In</button> 
+            <label>Email: </label>
+            <input
+            type='text'
+            placeholder='Enter your email'
+            value={email}
+            onChange={ (e) => setEmail(e.target.value) }
+            />
+
+            <label>Password: </label>
+            <input
+            type='password'
+            placeholder='Enter your password'
+            value={password}
+            onChange={ (e) => setPassword(e.target.value) }
+            />
+            <button type='button' onClick={register}>Register</button>
             </form>
-        </>
-    )
+        </div>
+    );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
-import Auth from "./auth"
+'use client'
+import Auth from "./auth";
+import PocketBase from 'pocketbase';
+
 
 export default function Home() {
+  const pb = new PocketBase('http://127.0.0.1:8090');
+
   return (
-    <div>
-      <h1> HELLO </h1>
+    <main>
+      {/* <h1>Logged in : {pb.authStore.isValid.toString()} </h1> */}
       <Auth />
-    </div>
+    </main>
   )
 }
